6c8156ba14090f7bb8c4385f56c17fc68c378340
MoseyByrne <mosey.b.byrne@gmail.com>
2022-09-11 12:23:23 -0700

    add import and export statements

---
+++ src/components/CoffeeControl.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CoffeeList from "./CoffeeList";
+import NewCoffeeForm from "./NewCoffeeForm";
+import CoffeeDetail from "./CoffeeDetail";
+
+class CoffeeControl extends React.Component {
+
+}
+
+export default CoffeeControl;
---
+++ src/components/NewCoffeeForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from 'uuid';
+
+function NewCoffeeForm(props) {
+
+}
+
+export default NewCoffeeForm;
